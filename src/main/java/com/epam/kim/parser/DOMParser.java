2ef2f9c827b90b5cfe1393073cc88910c02fb637
coppokim <vasiliy0303@mail.ru>
2016-11-06 15:48:55 +0600

    corrected DOM-parser

--- src/main/java/com/epam/kim/parser/DOMParser.java
+++ src/main/java/com/epam/kim/parser/DOMParser.java
@@ -1,80 +1,54 @@
 package com.epam.kim.parser;
 
-import com.epam.kim.entity.Product;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.lang.model.element.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.File;
 
 public class DOMParser {
     public static void parse () {
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-
-        DocumentBuilder builder = null;
         try {
-            builder = factory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            System.out.println("Error with builder");
-            e.printStackTrace();
-        }
-
-        //TODO fix this error
-        Document document = null;
-        try {
-            document = builder.parse(ClassLoader.getSystemResourceAsStream("src\\main\\resources\\xml\\products.xml"));
-        } catch (SAXException e) {
-            System.out.println("Error with SAXParser");
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("IO Error");
-            e.printStackTrace();
-        }
-        ArrayList<Product> prodList = new ArrayList<Product>();
-
-        NodeList nodeList = document.getDocumentElement().getChildNodes();
-        for (int i = 0; i < nodeList.getLength(); i++) {
-
-            Node node = (Node) nodeList.item(i);
-            if (node instanceof Element) {
-                Product prod = new Product();
-
-                NodeList childNodes = node.getChildNodes();
-                for (int j = 0; j < childNodes.getLength(); j++) {
-                    Node cNode = (Node) childNodes.item(j);
-
-                    if (cNode instanceof Element) {
-                        String content = cNode.getLastChild().
-                                getTextContent().trim();
-                        String s = cNode.getNodeName();
-                        if (s.equals("name")) {
-                            prod.setName(content);
-
-                        } else if (s.equals("price")) {
-                            prod.setPrice(Integer.parseInt(content));
-
-                        } else if(s.equals("id")){
-                            prod.setId(Byte.parseByte(content));
-                        }
-                    }
+            File inputFile = new File("src\\main\\resources\\xml\\products.xml");
+            DocumentBuilderFactory dbFactory
+                    = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element :"
+                    + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("product");
+            System.out.println("----------------------------");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                System.out.println("\nCurrent Element :"
+                        + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("id : "
+                            + eElement
+                            .getElementsByTagName("id")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("Name : "
+                            + eElement
+                            .getElementsByTagName("name")
+                            .item(0)
+                            .getTextContent());
+                    System.out.println("price : "
+                            + eElement
+                            .getElementsByTagName("price")
+                            .item(0)
+                            .getTextContent());
                 }
-                prodList.add(prod);
             }
-
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-
-        for (Product prod : prodList) {
-            System.out.println(prod);
-        }
-
     }
 
 }
