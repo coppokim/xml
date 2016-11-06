d1f055ca2fd8e77b4961c44daa13fb3614a32380
coppokim <vasiliy0303@mail.ru>
2016-11-06 17:59:18 +0600

    corrected StAX-parser

--- src/main/java/com/epam/kim/Main.java
+++ src/main/java/com/epam/kim/Main.java
@@ -2,16 +2,18 @@ package com.epam.kim;
 
 import com.epam.kim.parser.DOMParser;
 import com.epam.kim.parser.SaxParser;
+import com.epam.kim.parser.StAXParser;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLStreamException;
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws ParserConfigurationException, SAXException {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, XMLStreamException {
         SAXParserFactory factory = SAXParserFactory.newInstance();
         SAXParser parser = factory.newSAXParser();
         SaxParser saxp = new SaxParser();
@@ -23,6 +25,6 @@ public class Main {
         }
 
         DOMParser.parse();
-
+        StAXParser.parse();
     }
 }
--- src/main/java/com/epam/kim/parser/StAXParser.java
+++ src/main/java/com/epam/kim/parser/StAXParser.java
@@ -1,4 +1,76 @@
 package com.epam.kim.parser;
 
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
 public class StAXParser {
+    public static void parse() {
+        boolean bId = false;
+        boolean bName = false;
+        boolean bPrice = false;
+        try {
+            XMLInputFactory factory = XMLInputFactory.newInstance();
+            XMLEventReader eventReader =
+                    factory.createXMLEventReader(
+                            new FileReader("src\\main\\resources\\xml\\products.xml"));
+
+            while(eventReader.hasNext()){
+                XMLEvent event = eventReader.nextEvent();
+                switch(event.getEventType()){
+                    case XMLStreamConstants.START_ELEMENT:
+                        StartElement startElement = event.asStartElement();
+                        String qName = startElement.getName().getLocalPart();
+                        if (qName.equalsIgnoreCase("product")) {
+                            System.out.println("Start Element : product");
+
+                        } else if (qName.equalsIgnoreCase("id")) {
+                            bId = true;
+                        } else if (qName.equalsIgnoreCase("name")) {
+                            bName = true;
+                        } else if (qName.equalsIgnoreCase("price")) {
+                            bPrice = true;
+                        }
+                        break;
+                    case XMLStreamConstants.CHARACTERS:
+                        Characters characters = event.asCharacters();
+                        if(bId){
+                            System.out.println("Id : "
+                                    + characters.getData());
+                            bId = false;
+                        }
+                        if(bName){
+                            System.out.println("Name: "
+                                    + characters.getData());
+                            bName = false;
+                        }
+                        if(bPrice){
+                            System.out.println("Price: "
+                                    + characters.getData());
+                            bPrice = false;
+                        }
+                        break;
+                    case  XMLStreamConstants.END_ELEMENT:
+                        EndElement endElement = event.asEndElement();
+                        if(endElement.getName().getLocalPart().equalsIgnoreCase("student")){
+                            System.out.println("End Element : student");
+                            System.out.println();
+                        }
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (XMLStreamException e) {
+            e.printStackTrace();
+        }
+    }
 }
